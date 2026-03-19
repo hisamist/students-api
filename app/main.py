07206bc9a0eb7f95190a5e7671b97f04c695ca88
hisamist <hisamistolz@gmail.com>
2026-03-19 12:34:45 +0100

    feat(api): add GET endpoints for all students & reset db

--- app/main.py
+++ app/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from app.data import students_db, reset_db
 
-app = FastAPI()
+app = FastAPI(title="Students API")
 
-@app.get("/")
-def read_root():
-    return {"message": "EdTech API is running"}
+@app.get("/students")
+async def get_all_students():
+    return students_db
+
+@app.post("/reset")
+async def perform_reset():
+    reset_db()
+    return {"message": "Données réinitialisées avec succès"}
